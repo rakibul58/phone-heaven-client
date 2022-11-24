7e8665c5870dd3a14470d20e30b25fb681cd1653
rakibul58 <rhrahi14@gmail.com>
2022-11-24 13:38:55 +0600

    error page added

--- src/Pages/Shared/ErrorPage/ErrorPage.js
+++ src/Pages/Shared/ErrorPage/ErrorPage.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import MyButton from '../../../components/MyButton/MyButton';
 
 const ErrorPage = () => {
     return (
-        <div>
-            
-        </div>
+        <section className="flex items-center h-full p-16">
+            <div className="container flex flex-col items-center justify-center px-5 mx-auto my-8">
+                <div className="max-w-md text-center">
+                    <h2 className="mb-8 font-extrabold text-9xl text-accent">
+                        <span className="sr-only">Error</span>404
+                    </h2>
+                    <p className="text-2xl font-semibold md:text-3xl">Sorry, we couldn't find this page.</p>
+                    <p className="mt-4 mb-8 text-white">But dont worry, you can find plenty of other things on our homepage.</p>
+                    <Link to='/'><MyButton>Back to homepage</MyButton></Link>
+                </div>
+            </div>
+        </section>
     );
 };
 
