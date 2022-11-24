7bb9dee484c54543bbe079e12e8e88070480b70f
rakibul58 <rhrahi14@gmail.com>
2022-11-24 13:27:54 +0600

    navbar changed

--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import img from '../../../assets/navbar/no_image-removebg-preview.png';
 
 const Navbar = () => {
+    const navElements = <>
+        <li><Link to='/login'>Login</Link></li>
+    </>
     return (
         <div className="navbar bg-base-100 py-5">
             <div className="navbar-start">
@@ -9,41 +13,17 @@ const Navbar = () => {
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
-                    <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                        <li><Link>Item 1</Link></li>
-                        <li tabIndex={0}>
-                            <Link className="justify-between">
-                                Parent
-                                <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" /></svg>
-                            </Link>
-                            <ul className="p-2">
-                                <li><Link>Submenu 1</Link></li>
-                                <li><Link>Submenu 2</Link></li>
-                            </ul>
-                        </li>
-                        <li><Link>Item 3</Link></li>
+                    <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52 font-semibold text-accent text-xl">
+                        {navElements}
                     </ul>
                 </div>
-                <Link className="btn btn-ghost normal-case text-xl">daisyUI</Link>
+                <img className='w-[80px] mr-[-45px]' src={img} alt="" />
+                <Link className="btn btn-ghost normal-case text-3xl bg-clip-text bg-gradient-to-r from-accent to-secondary text-transparent font-extrabold">Phone Heaven</Link>
             </div>
-            <div className="navbar-center hidden lg:flex">
+            <div className="navbar-end hidden lg:flex font-semibold text-accent text-xl">
                 <ul className="menu menu-horizontal p-0">
-                    <li><Link>Item 1</Link></li>
-                    <li tabIndex={0}>
-                        <Link>
-                            Parent
-                            <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg>
-                        </Link>
-                        <ul className="p-2">
-                            <li><Link>Submenu 1</Link></li>
-                            <li><Link>Submenu 2</Link></li>
-                        </ul>
-                    </li>
-                    <li><Link>Item 3</Link></li>
+                    {navElements}
                 </ul>
-            </div>
-            <div className="navbar-end">
-                <Link className="btn">Get started</Link>
             </div>
         </div>
     );
